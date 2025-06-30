d73ab192624cc4177567ca6c6d9c00b7a3f34400
Arielle Bello <ariellenbello@gmail.com>
2025-06-30 18:18:33 +0000

    Add all project code

--- code/newapidaytwo.py
+++ code/newapidaytwo.py
@@ -1,5 +1,8 @@
 import requests 
 
+
+# star wars api test for names of people 
+
 BASE_URL = 'https://swapi.py4e.com/api/'
 data = {'search': 'r2'}
 response = requests.get(BASE_URL + 'people/', data=data) 
